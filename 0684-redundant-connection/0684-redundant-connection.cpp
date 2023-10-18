73e0235bfb7babc672aa01a0cbe56926a83f70a5
Princy Malhotra <92246333+Princy-Malhotra@users.noreply.github.com>
2023-10-18 22:35:31 +0530

    Time: 3 ms (93.39%), Space: 9.7 MB (24.47%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,38 +1,51 @@
-class Solution {
+class DSU{
 public:
-    void dfs(vector<vector<int>>& adj, int src, unordered_map<int,int>& vis){
-        vis[src]=1;
-        for(auto x:adj[src]){
-            if(!vis[x]){
-                dfs(adj,x,vis);
+    int n;
+    vector<int>parent;
+    DSU(int n){
+        this->n = n;
+        parent.resize(n+1,0);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int x){
+        while(parent[x]!=x){
+            x = parent[x];
+        }
+        return x;
+    }
+    void unionxy(int x,int y){
+        int p1 = find(x);
+        int p2 = find(y);
+        if(p1==p2){
+            return;
+        }
+        else{
+            if(p1<p2){
+                parent[p2] = p1;
+            }
+            else{
+                parent[p1] = p2;
             }
         }
-        return;
     }
-    bool help(vector<vector<int>>& adj, int src, int dest){
-        unordered_map<int,int>vis;
-        dfs(adj,src,vis);
-        if(!vis[dest]){
-            return true;
-        }
-        return false;
-    }
+};
+
+class Solution {
+public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
         vector<vector<int>>adj(n);
         vector<int>ans(2);
+        DSU dsu(n);
         for(auto x:edges){
-            int src = x[0] - 1;
-            int dest = x[1] - 1;
-            bool t = help(adj,src,dest);
-            if(t){
-                adj[x[0]-1].push_back(x[1]-1);
-                adj[x[1]-1].push_back(x[0]-1);
-            }
-            else{
+            if(dsu.find(x[0])==dsu.find(x[1])){
                 ans[0] = x[0];
                 ans[1] = x[1];
             }
+            else
+            dsu.unionxy(x[0],x[1]);
         }
         return ans;
     }
