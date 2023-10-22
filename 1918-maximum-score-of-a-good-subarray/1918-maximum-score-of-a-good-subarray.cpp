f1d9d8ece3003b4329420babd0a3b8099322dbfb
Princy Malhotra <92246333+Princy-Malhotra@users.noreply.github.com>
2023-10-22 18:27:47 +0530

    Time: 143 ms (58.11%), Space: 89.8 MB (64.25%) - LeetHub

--- 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -2,35 +2,111 @@ class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
         int n=nums.size();
+        // int ans = nums[k];
+        // int currpro = nums[k];
+        // priority_queue<int,vector<int>,greater<int>>pq;
+        // pq.push(nums[k]);
+        // int i=k,j=k;
+        // while(i!=0 && j!=n-1){
+        //         if(nums[i-1]>nums[j+1]){
+        //             i--;
+        //             pq.push(nums[i]);
+        //             currpro = (pq.top())*(j-i+1);
+        //             ans = max(ans,currpro);
+        //         }
+        //         else{
+        //             j++;
+        //             pq.push(nums[j]);
+        //             currpro = (pq.top())*(j-i+1);
+        //             ans = max(ans,currpro);
+        //         }    
+        // }
+        // while(i!=0){
+        //     i--;
+        //     pq.push(nums[i]);
+        //     currpro = (pq.top())*(j-i+1);
+        //     ans = max(ans,currpro);
+        // }
+        // while(j!=n-1){
+        //     j++;
+        //     pq.push(nums[j]);
+        //     currpro = pq.top()*(j-i+1);
+        //     ans = max(ans,currpro);
+        // }
+        // return ans;
         int ans = nums[k];
         int currpro = nums[k];
-        priority_queue<int,vector<int>,greater<int>>pq;
-        pq.push(nums[k]);
+        stack<int>s;
         int i=k,j=k;
+        s.push(nums[k]);
         while(i!=0 && j!=n-1){
-                if(nums[i-1]>nums[j+1]){
-                    i--;
-                    pq.push(nums[i]);
-                    currpro = (pq.top())*(j-i+1);
-                    ans = max(ans,currpro);
+            if(nums[i-1]>nums[j+1]){
+                i--;
+                if(s.empty()){
+                    s.push(nums[i]);
+                    currpro = nums[i] * (j-i+1);
                 }
                 else{
-                    j++;
-                    pq.push(nums[j]);
-                    currpro = (pq.top())*(j-i+1);
-                    ans = max(ans,currpro);
-                }    
+                    while(!s.empty() && s.top()>nums[i]){
+                        s.pop();
+                    }
+                    if(s.empty()){
+                        s.push(nums[i]);
+                    }
+                    currpro = s.top() * (j-i+1);
+                }
+                ans = max(ans,currpro);
+            }
+            else{
+                j++;
+                if(s.empty()){
+                    s.push(nums[j]);
+                    currpro = nums[j]*(j-i+1);
+                }
+                else{
+                    while(!s.empty() && s.top()>nums[j]){
+                        s.pop();
+                    }
+                    if(s.empty()){
+                        s.push(nums[j]);
+                    }
+                    currpro = s.top() * (j-i+1);
+                }
+                ans = max(ans,currpro);
+            }
         }
         while(i!=0){
             i--;
-            pq.push(nums[i]);
-            currpro = (pq.top())*(j-i+1);
+            if(s.empty()){
+                s.push(nums[i]);
+                currpro = nums[i] * (j-i+1);
+            }
+            else{
+                while(!s.empty() && s.top()>nums[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    s.push(nums[i]);
+                }
+                currpro = s.top() * (j-i+1);
+            }
             ans = max(ans,currpro);
         }
         while(j!=n-1){
             j++;
-            pq.push(nums[j]);
-            currpro = pq.top()*(j-i+1);
+            if(s.empty()){
+                s.push(nums[j]);
+                currpro = nums[j]*(j-i+1);
+            }
+            else{
+                while(!s.empty() && s.top()>nums[j]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    s.push(nums[j]);
+                }
+                currpro = s.top() * (j-i+1);
+            }
             ans = max(ans,currpro);
         }
         return ans;
